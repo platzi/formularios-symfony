788f9fa888eddd4d6a109b85d854554961af649b
Italo Morales F <italomoralesf@MacBook-Pro-de-Italo.local>
2022-05-04 13:46:44 -0500

    Tipos de controles, personalización y CSRF

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,13 +7,26 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('title', TextType::class, [
+                'label' => 'Título de la publicación',
+                'help'  => 'Piensa en el SEO ¿Cómo buscarías en Google?'
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Contenido',
+                'attr'  => ['rows' => 9, 'class' => 'bg-light']
+            ])
+            ->add('Enviar', SubmitType::class, [
+                'attr' => ['class' => 'btn-dark']
+            ])
         ;
     }
 
@@ -21,6 +34,8 @@ class PostType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Post::class,
+            // 'csrf_protection' => false
+            // 'csrf_field_name' => '_token_personalizado'
         ]);
     }
 }
