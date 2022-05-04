3b716cfcc4913f04d8f9149ac11d8b2b42a32193
Italo Morales F <profesoritalomoralesf@gmail.com>
2022-05-04 16:12:35 -0500

    Guardar

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,17 +3,28 @@
 namespace App\Controller;
 
 use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post/crear', name: 'post_create')]
-    public function create(): Response
+    #[Route('/post/crear', name:'post_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostType::class);
+
+        $form->handleRequest($request);
+        if ( $form->isSubmitted() ) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Publicación guardada con éxito');
+            return $this->redirectToRoute('post_create');
+        }
 
         return $this->render('post/create.html.twig', [
             'form' => $form->createView(),
